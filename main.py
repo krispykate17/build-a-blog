84c72cb58dd6e41a597e6496e3d003dc2a361540
Katie Juhala <kajuhala@gmail.com>
2017-04-13 13:38:57 -0500

    When user submits new post they are directed to their individual post

--- main.py
+++ main.py
@@ -15,11 +15,70 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    blog_content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class NewPost(webapp2.RequestHandler):
+    def render_NewPost(self, title="", blog_content="", error=""):
+        t = jinja_env.get_template("new_posts.html")
+        content = t.render(title=title, blog_content=blog_content, error=error)
+        self.response.write(content)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_NewPost()
+
+    def post(self):
+        title = self.request.get("title")
+        blog_content = self.request.get("blog_content")
+
+        if title and blog_content:
+            b = Blog(title=title, blog_content=blog_content)
+            b.put()
+            blog_id = str(b.key().id())
+            #b.get_by_id()
+
+            self.redirect("/blog/" + blog_id)
+        else:
+            error = "We need both a title and some content"
+            self.render_NewPost(title, blog_content, error)
+
+class TopFive(webapp2.RequestHandler):
+    def render_TopFive(self, title="", blog_content="", error=""):
+        top_five = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+
+        t = jinja_env.get_template("main_blog.html")
+        content = t.render(title=title, blog_content=blog_content, error=error,
+                            top_five=top_five)
+        self.response.write(content)
+
+    def get(self):
+        self.render_TopFive()
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def render_IndivBlog(self, int_id):
+        indivBlog = Blog.get_by_id(int_id)
+
+        t = jinja_env.get_template("indivblog.html")
+        content = t.render(blog= indivBlog)
+        self.response.write(content)
+
+    def get(self, id):
+        int_id = int(id)
+        self.render_IndivBlog(int_id)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', NewPost),
+    ('/blog', TopFive),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
